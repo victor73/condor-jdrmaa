48de13fda425b9dc883203eec810053778f59e8c
victor73 <victor73@6d71838b-fb50-0410-911a-e0ffa7579501>
2008-08-16 02:31:13 +0000

    Run in the user's home directory.

--- src/net/sf/igs/test/SimpleTest.java
+++ src/net/sf/igs/test/SimpleTest.java
@@ -42,6 +42,9 @@ public class SimpleTest {
 			session.init("");
 			JobTemplate jt = session.createJobTemplate();
 
+			// Note: The test will fail if the user's home directory does not
+			// exist on the execute node.
+			jt.setWorkingDirectory(System.getProperty("user.home"));
 			jt.setRemoteCommand("/bin/sleep");
 			jt.setArgs(Collections.singletonList("10"));
 			jt.setJobName(SimpleTest.class.getSimpleName());
