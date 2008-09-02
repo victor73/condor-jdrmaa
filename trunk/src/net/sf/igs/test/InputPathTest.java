7fbe952d7b23ea17a764a933667baab6412a3bb5
victor73 <victor73@6d71838b-fb50-0410-911a-e0ffa7579501>
2008-09-02 14:26:54 +0000

    New test

--- trunk/src/net/sf/igs/test/InputPathTest.java
+++ trunk/src/net/sf/igs/test/InputPathTest.java
@@ -16,15 +16,16 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.util.Collections;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.ggf.drmaa.JobTemplate;
 import org.ggf.drmaa.Session;
@@ -34,60 +35,93 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * Tests whether support for the {@link JobTemplate#setOutputPath(String) setOutputPath} method really works.
+ * Tests whether support for the {@link JobTemplate#setInputPath(String)
+ * setInputPath} method really works. The test operates by generating an input
+ * file with some text in it (the name of the test class) and then invoking the
+ * "cat" command with the file as the input. The output file should then have
+ * the same text as the input file.
+ * 
+ * @see "cat command man page"
  */
-public class OutputPathTest {
-	private static String name = OutputPathTest.class.getSimpleName();
-	private static File outputFile = null;
-	private static String outputPath;
+public class InputPathTest {
+	private static String name = InputPathTest.class.getSimpleName();
+	private static File inputFile, outputFile = null;
+	private static String inputPath, outputPath = null;
 	
 	/**
 	 * Tests whether support for the {@link JobTemplate#setOutputPath(String) setOutputPath}
 	 * works properly.
+	 * @throws IOException 
 	 */
 	@BeforeClass
-	public static void setup() {
+	public static void setup() throws IOException {
 		// Let's configure the output path for where the STDOUT of the job should be saved to.
+		inputPath = System.getProperty("user.home") + File.separator + name + ".txt";
 		outputPath = System.getProperty("user.home") + File.separator + name + ".out";
-		
+		inputFile = new File(inputPath);
 		outputFile = new File(outputPath);
 		
-		deleteOutputFile();
+		// Delete any file that may still be around from previous tests. The content might
+		// not be what we want in there...
+		deleteInputFile();
+		
+		// Create the input file with what we know will have the right content for the test
+		createInputFile();
 	}
 
-	private static void deleteOutputFile() {
+	// If the input file exists, delete it.
+	private static void deleteInputFile() {
 		// Delete the file if it's there (could be a leftover from previous tests)
-		if (outputFile != null && outputFile.exists()) {
+		if (inputFile != null && inputFile.exists()) {
 			boolean deleted = false;
-			
 			do {
-				deleted = outputFile.delete();
+				deleted = inputFile.delete();
+				if (! deleted) {
+					System.err.println("Unable to delete file " + inputFile.getAbsolutePath());
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						System.err.println("Test interrupted.");
+						break;
+					}
+				}
 			} while (! deleted);
 		}
 	}
 	
+	// Create the file that will act as the input file for this test and
+	// write some content into the file. Then close the file in preparation
+	// for the test execution.
+	private static void createInputFile() throws IOException {
+		File input = new File(inputPath);
+		input.createNewFile();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(input));
+		writer.write(name);
+		writer.newLine();
+		writer.close();
+	}
+	
 	/**
-	 * Test the {@link JobTemplate#setOutputPath(String) setOutputPath} method.
+	 * Test the {@link JobTemplate#setInputPath(String) setInputPath} method.
 	 */
 	@Test
-	public void testOutputPath() {
+	public void testInputPath() {
 		try {
 			Session session = SessionFactory.getFactory().getSession();
 			session.init(name);
 			JobTemplate jt = session.createJobTemplate();
 
-			String argument = "abcdefg";
-			
-			jt.setRemoteCommand("/bin/echo");
-			jt.setArgs(Collections.singletonList(argument));
+			jt.setRemoteCommand("/bin/cat");
 			
 			// Set the job name
 			jt.setJobName(name);
 
-			// Make sure we don't have the file around from previous test invocations
-			assertFalse(outputFile.exists());
+			// Make sure we have the input file for the test
+			assertTrue(inputFile.exists());
 			
+			jt.setInputPath(":" + inputPath);
 			jt.setOutputPath(":" + outputPath);
+			
 			String jobId = session.runJob(jt);
 			assertNotNull(jobId);
 			assertTrue(jobId.length() > 0);
@@ -112,7 +146,7 @@ public class OutputPathTest {
 			BufferedReader reader = new BufferedReader(new FileReader(outputFile));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				if (line.contains(argument)) {
+				if (line.contains(name)) {
 					correctOutput = true;
 					break;
 				}
@@ -127,10 +161,13 @@ public class OutputPathTest {
 	}
 	
 	/**
-	 * Runs after the test is complete and removes the output file.
+	 * Runs after the test is complete and removes the input and output files.
 	 */
 	@AfterClass
 	public static void cleanup() {
-		deleteOutputFile();
+		deleteInputFile();
+		if (outputFile.exists()) {
+			outputFile.delete();
+		}
 	}
 }
